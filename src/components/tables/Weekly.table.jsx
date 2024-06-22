4b301d2d47eed15529654e924a73f62ed88473f0
HtooKhant-vexix <vexixongaming02@gmail.com>
2024-06-22 22:13:04 +0700

    G

--- src/components/tables/Weekly.table.jsx
+++ src/components/tables/Weekly.table.jsx
@@ -1,129 +1,168 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { useReactToPrint } from 'react-to-print';
-import { useDownloadExcel } from 'react-export-table-to-excel';
-import { RiFileExcel2Fill } from 'react-icons/ri';
-import { AiFillPrinter } from 'react-icons/ai';
+import React, { useEffect, useRef, useState } from "react";
+import { useReactToPrint } from "react-to-print";
+import { useDownloadExcel } from "react-export-table-to-excel";
+import { RiFileExcel2Fill } from "react-icons/ri";
+import { AiFillPrinter } from "react-icons/ai";
 
-function WeeklyTable({ calenderOne, calenderTwo,okData,isSearch,selectedStation,language }) {
-    const [n2, Setn2] = useState(0);
-    const [n5, Setn5] = useState(0);
-    const [hsd, Sethsd] = useState(0);
-    const [phsd, Setphsd] = useState(0);
-    const [time, setTime] = useState(0);
-    const tRef  = useRef();
-  const [station, setStation] = useState('');
-  const [licenseNo, setLicenseNo] = useState('');
+function WeeklyTable({
+  calenderOne,
+  calenderTwo,
+  okData,
+  capacity,
+  isSearch,
+  selectedStation,
+  language,
+}) {
+  const [n2, Setn2] = useState(0);
+  const [n5, Setn5] = useState(0);
+  const [hsd, Sethsd] = useState(0);
+  const [phsd, Setphsd] = useState(0);
+  const [time, setTime] = useState(0);
+  const tRef = useRef();
+  const [station, setStation] = useState("");
+  const [licenseNo, setLicenseNo] = useState("");
 
-    useEffect(() => {
-        let ninety2 = 0
-    let ninety5 = 0
-    let diesel = 0
-    let premium = 0
-      let stationName = '';
-      let license = '';
-        
-        const Time = new Date(calenderTwo.getDate()) - new Date(calenderOne.getDate());
+  useEffect(() => {
+    let ninety2 = 0;
+    let ninety5 = 0;
+    let diesel = 0;
+    let premium = 0;
+    let stationName = "";
+    let license = "";
 
-        setTime(Time);
-       
-        
-      okData.forEach((obj) => {
-        stationName = obj.stationDetailId.name;
-        license = obj.stationDetailId.lienseNo;
-             if (obj.fuelType === "005-Premium Diesel") {
-       premium += obj.saleLiter
+    console.log(okData, ".//.");
+
+    const Time =
+      new Date(calenderTwo.getDate()) - new Date(calenderOne.getDate());
+
+    setTime(Time);
+
+    okData.forEach((obj) => {
+      stationName = obj.stationDetailId.name;
+      license = obj.stationDetailId.lienseNo;
+      if (obj.fuelType === "005-Premium Diesel") {
+        premium += obj.saleLiter;
       }
       if (obj.fuelType === "004-Diesel") {
-        diesel += obj.saleLiter
+        diesel += obj.saleLiter;
       }
       if (obj.fuelType === "001-Octane Ron(92)") {
-        ninety2 += obj.saleLiter
+        ninety2 += obj.saleLiter;
       }
       if (obj.fuelType === "002-Octane Ron(95)") {
-        ninety5 += obj.saleLiter
+        ninety5 += obj.saleLiter;
       }
-        })
+    });
 
-        Setn2(ninety2);
-        Setn5(ninety5);
-        Sethsd(diesel);
-      Setphsd(premium);
-      setStation(stationName);
-      setLicenseNo(license);
-        
-    }, [okData, calenderOne, calenderTwo])
-  
-  
+    Setn2(ninety2);
+    Setn5(ninety5);
+    Sethsd(diesel);
+    Setphsd(premium);
+    setStation(stationName);
+    setLicenseNo(license);
+  }, [okData, calenderOne, calenderTwo]);
+
+  const state = okData[0]?.stationDetailId.location.split(",");
+
   const handlePrint = useReactToPrint({
-    content:()=>tRef.current
-  })
+    content: () => tRef.current,
+  });
 
-   const { onDownload } = useDownloadExcel({
+  const { onDownload } = useDownloadExcel({
     currentTableRef: tRef.current,
     filename: `Weekly Sale Report`,
-    sheet:  `Weekly Sale Report`
-   })
+    sheet: `Weekly Sale Report`,
+  });
 
   return (
-      <div className='mt-[60px]'>
-          <table ref={tRef}>
-              <thead>
-                  <tr>
-                      <th rowSpan={2}>{language.no}</th>
-                      <th rowSpan={2}>{language.import_company}</th>
-                      <th rowSpan={2}>{language.company_name}</th>
-                      <th rowSpan={2}>{language.station_name}</th>
-                      {/* <th rowSpan={2}>Location</th> */}
-                      <th rowSpan={2}>{language.pprd_license}</th>
-                      <th colSpan={4}>
-                        {
-                language.no === "စဉ်" ? <p>{calenderOne.toDateString()}  {language.to} {calenderTwo.toDateString()} {language.total_sale}</p>:<p>Total Sale  from  {calenderOne.toDateString()}  to {calenderTwo.toDateString()}</p>
-                        }
-                        </th>
-                      <th colSpan={4}>{language.average_sale_per_day}</th>
+    <div className="mt-[60px] relative w-full overflow-x-scroll h-[230px]">
+      <table ref={tRef} className="absolute w-[2500px]">
+        <thead>
+          <tr>
+            {/* <th rowSpan={2}>{language.no}</th> */}
+            <th rowSpan={2}>{language.import_company}</th>
+            <th rowSpan={2}>{language.company_name}</th>
+            <th rowSpan={2}>{language.station_name}</th>
+            {/* <th rowSpan={2}>Location</th> */}
+            <th rowSpan={2}>{language.pprd_license}</th>
+            <th rowSpan={2}>{language.Township}</th>
+            <th rowSpan={2}>{language.State}</th>
+            <th rowSpan={2}>{language.own}</th>
+            <th colSpan={4}>{language.average_sale_per_day}</th>
+            <th colSpan={4}>
+              {language.no === "စဉ်" ? (
+                <p>
+                  {calenderOne.toDateString()} {language.to}{" "}
+                  {calenderTwo.toDateString()} {language.total_sale}
+                </p>
+              ) : (
+                <p>
+                  From {calenderOne.toDateString()} to{" "}
+                  {calenderTwo.toDateString()} Total Sale Amount (Gallon)
+                </p>
+              )}
+            </th>
 
-
-                  </tr>
-                  <tr>
-                      <th>001-Octane Ron(92) (Li)</th>
-                      <th>002-Octane Ron(95) (Li)</th>
-                      <th>004-Diesel (Li)</th>
-                      <th>005-Premium Diesel (Li)</th>
-                       <th>001-Octane Ron(92) (Li)</th>
-                      <th>002-Octane Ron(95) (Li)</th>
-                      <th>004-Diesel (Li)</th>
-                      <th>005-Premium Diesel (Li)</th>
-
-
-                  </tr>
-              </thead>
-              <tbody>
-                  <tr>
-                      <td>1</td>
-                      <td>Kyaw San</td>
-                      <td>Kyaw San</td>
-                      <td>{station}</td>
-                      {/* <td>Kyawe Tat Sone Village, Thazi Township</td> */}
-                      <td>{licenseNo}</td>
-                      <td>{n2?n2.toFixed(3):'-'}</td>
-                      <td>{n5?n5.toFixed(3):"-"}</td>
-                      <td>{hsd?hsd.toFixed(3):"-"}</td>
-                      <td>{phsd?phsd.toFixed(3):'-'}</td>
-                      <td>{time === 0 ? n2.toFixed(3):(n2 / time).toFixed(3)}</td>
-                      <td>{time === 0 ? n5.toFixed(3):(n5 / time).toFixed(3)}</td>
-                      <td>{time === 0 ? hsd.toFixed(3):(hsd / time).toFixed(3)}</td>
-                      <td>{time === 0 ? phsd.toFixed(3):(phsd / time).toFixed(3)}</td>
-
-
-                  </tr>
-              </tbody>
+            <th colSpan={4}>{language.average_sale_per_day}</th>
+            <th colSpan={4}>{language.average_sale_per_day}</th>
+            <th rowSpan={2}>{language.no}</th>
+            <th rowSpan={2}>{language.no}</th>
+          </tr>
+          <tr>
+            {capacity.map((e) => (
+              <th>{e.fuelType}</th>
+            ))}
+            {capacity.map((e) => (
+              <th>{e.fuelType}</th>
+            ))}
+            {capacity.map((e) => (
+              <th>{e.fuelType}</th>
+            ))}
+            {capacity.map((e) => (
+              <th>{e.fuelType}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {/* <td>1</td> */}
+            <td>Kyaw San</td>
+            <td>Kyaw San</td>
+            <td>{station}</td>
+            {/* <td>Kyawe Tat Sone Village, Thazi Township</td> */}
+            <td>{licenseNo}</td>
+            <td className=" text-left">{state[0]}</td>
+            <td className=" text-center">{state[state.length - 1]}</td>
+            <td className=" text-center">Partner Shop</td>
+            <td>{time === 0 ? n2.toFixed(3) : (n2 / time).toFixed(3)}</td>
+            <td>{time === 0 ? n5.toFixed(3) : (n5 / time).toFixed(3)}</td>
+            <td>{time === 0 ? hsd.toFixed(3) : (hsd / time).toFixed(3)}</td>
+            <td>{time === 0 ? phsd.toFixed(3) : (phsd / time).toFixed(3)}</td>
+            <td>{n2 ? n2.toFixed(3) : "0.00"}</td>
+            <td>{n5 ? n5.toFixed(3) : "0.00"}</td>
+            <td>{hsd ? hsd.toFixed(3) : "0.00"}</td>
+            <td>{phsd ? phsd.toFixed(3) : "0.00"}</td>
+          </tr>
+        </tbody>
       </table>
-         <div className='flex p-3  text-[16px] mt-[30px] mb-[50px] items-center justify-start gap-3'>
-        <button onClick={() => onDownload()} className='flex items-center justify-center gap-2 text-md' >{language.toExcel}<RiFileExcel2Fill size={30} /></button>
-        <button onClick={handlePrint} className='flex items-center justify-center gap-2 text-md' >{language.toPrint}<AiFillPrinter size={30}/></button>
-          </div>
+      <div className="flex p-3  text-[16px] mt-[30px] mb-[50px] items-center justify-start gap-3">
+        <button
+          onClick={() => onDownload()}
+          className="flex items-center justify-center gap-2 text-md"
+        >
+          {language.toExcel}
+          <RiFileExcel2Fill size={30} />
+        </button>
+        <button
+          onClick={handlePrint}
+          className="flex items-center justify-center gap-2 text-md"
+        >
+          {language.toPrint}
+          <AiFillPrinter size={30} />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default WeeklyTable
+export default WeeklyTable;
--- src/pages/FuelBalanceReport.jsx
+++ src/pages/FuelBalanceReport.jsx
@@ -1,28 +1,29 @@
-import React, {useRef,useState,useEffect  } from 'react'
-import 'react-calendar/dist/Calendar.css';
+import React, { useRef, useState, useEffect } from "react";
+import "react-calendar/dist/Calendar.css";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
 // TankBalanceReport
-import 'react-calendar/dist/Calendar.css';
-import { LogoutUser } from '../redux/slices/LoginSlice';
-import Loading from '../components/Loading';
-import PageContainer from '../components/PageComponents/PageContainer';
-import InputContainer from '../components/PageComponents/InputContainer';
-import CalenderComponent from '../components/PageComponents/CalenderComponent';
-import FuelTypeComponent from '../components/PageComponents/FuelTypeComponent';
-import StationComponent from '../components/PageComponents/StationComponent';
-import TankComponent from '../components/PageComponents/TankComponent';
-import { FiSearch } from 'react-icons/fi';
-import FuelBalanceReportTable from '../components/tables/FuelBalanceReport.table';
-import { fetchFuelBalanceByTimeRange, getAllKyawSan027DailySaleReports, removeOldDats } from '../redux/slices/KyawSan027Slice';
-import { FcInfo } from 'react-icons/fc';
-import { EnglishFuelBalance } from '../Language/English/englishFuelBalanceReport';
-import { MyanmarFuelBalanceRport } from '../Language/Myanmar/myanmarFuelBalanceReport';
-
-
-
+import "react-calendar/dist/Calendar.css";
+import { LogoutUser } from "../redux/slices/LoginSlice";
+import Loading from "../components/Loading";
+import PageContainer from "../components/PageComponents/PageContainer";
+import InputContainer from "../components/PageComponents/InputContainer";
+import CalenderComponent from "../components/PageComponents/CalenderComponent";
+import FuelTypeComponent from "../components/PageComponents/FuelTypeComponent";
+import StationComponent from "../components/PageComponents/StationComponent";
+import TankComponent from "../components/PageComponents/TankComponent";
+import { FiSearch } from "react-icons/fi";
+import FuelBalanceReportTable from "../components/tables/FuelBalanceReport.table";
+import {
+  fetchFuelBalanceByTimeRange,
+  getAllKyawSan027DailySaleReports,
+  removeOldDats,
+} from "../redux/slices/KyawSan027Slice";
+import { FcInfo } from "react-icons/fc";
+import { EnglishFuelBalance } from "../Language/English/englishFuelBalanceReport";
+import { MyanmarFuelBalanceRport } from "../Language/Myanmar/myanmarFuelBalanceReport";
 
 let start = new Date();
 start.setHours(0);
@@ -34,110 +35,131 @@ function FuelBalanceReport() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-
   const tableRef = useRef();
-  const [loading, setloading] = useState(false)
-  const [okData, setOkData] = useState()
+  const [loading, setloading] = useState(false);
+  const [okData, setOkData] = useState();
   const [calenderOne, setCalenderOne] = useState(start);
-  const [fuelType, setFuelType] = useState({name: "All", code: "Please"});
-  const [tankName, setTankName] = useState({name: "All", code: "Please"});
-  const [selectedStation, setSelectedStation] = useState({name: "All", code: "Please"});
+  const [fuelType, setFuelType] = useState({ name: "All", code: "Please" });
+  const [tankName, setTankName] = useState({ name: "All", code: "Please" });
+  const [selectedStation, setSelectedStation] = useState({
+    name: "All",
+    code: "Please",
+  });
   const [language, setLanguage] = useState(EnglishFuelBalance);
   const [isSelectedStation, setIsSelectedStation] = useState(false);
   const datas = useSelector(getAllKyawSan027DailySaleReports);
-
- 
-
 
   useEffect(() => {
     if (!user.login) {
       navigate("/");
     }
-      if (user.language === "Myanmar" || user.language === "မြန်မာ") {
+    if (user.language === "Myanmar" || user.language === "မြန်မာ") {
       setLanguage(MyanmarFuelBalanceRport);
     } else if (user.language === "English" || user.language === "အင်္ဂလိပ်") {
       setLanguage(EnglishFuelBalance);
     }
     dispatch(removeOldDats());
     return () => {
-      dispatch(removeOldDats())
-    }
+      dispatch(removeOldDats());
+    };
   }, [navigate, user, dispatch]);
 
   const handleClick = () => {
-       if (calenderOne) {
-        if (selectedStation.code === "Please") {
-          setIsSelectedStation(true);
-        } else {
+    if (calenderOne) {
+      if (selectedStation.code === "Please") {
+        setIsSelectedStation(true);
+      } else {
+        setloading(true);
+        setIsSelectedStation(false);
+
+        const fetchData = async () => {
+          const bomb = [
+            user.token,
+            calenderOne,
+            selectedStation,
+            fuelType,
+            tankName,
+            user.accessDb,
+          ];
           setloading(true);
-          setIsSelectedStation(false);
-     
-          const fetchData = async () => {
-       
-            const bomb = [user.token, calenderOne, selectedStation,fuelType,tankName,user.accessDb];
-            setloading(true);
-            await dispatch(fetchFuelBalanceByTimeRange(bomb));
-            setloading(false);
-          }
-          fetchData();
-        }
+          await dispatch(fetchFuelBalanceByTimeRange(bomb));
+          setloading(false);
+        };
+        fetchData();
       }
-
-
     }
- 
-    useEffect(() => {
-        if (datas === "error") {
-          dispatch(LogoutUser());
-        }
-      if (datas?.result?.length > 0) {
-    let pureArray = [...datas.result]; // Create a shallow copy of the array
-    pureArray.sort((a, b) => a.tankNo - b.tankNo); // Sort the new array
-    setOkData(pureArray); // Update the state with the new sorted array
-       
+  };
+
+  useEffect(() => {
+    if (datas === "error") {
+      dispatch(LogoutUser());
+    }
+    if (datas?.result?.length > 0) {
+      let pureArray = [...datas.result]; // Create a shallow copy of the array
+      pureArray.sort((a, b) => a.tankNo - b.tankNo); // Sort the new array
+      setOkData(pureArray); // Update the state with the new sorted array
     } else {
-      setOkData([])
+      setOkData([]);
     }
-     
-    }, [datas, dispatch]);
-  
- 
+  }, [datas, dispatch]);
 
-
-
-    
   return (
- <PageContainer language={false}  title={language.main_title}>
+    <PageContainer language={false} title={language.main_title}>
       <InputContainer>
         <div className="flex flex-wrap gap-[20px]">
-        <CalenderComponent title={language.date} value={calenderOne} setValue={setCalenderOne} />
-        <FuelTypeComponent title={language.fuel_type} value={fuelType} setValue={setFuelType} />
-        <TankComponent language={language.tank_no} value={tankName} setValue={setTankName} /> 
-        <StationComponent title={language.station} value={selectedStation} setValue={setSelectedStation} />
-       
-        </div>  
-         {
-          isSelectedStation && <div className='flex mt-3 animate-[translate-y-6]   duration-200 text-blue-500 gap-[10px] justify-start text-[16px] items-center'><FcInfo/> Please Select Station</div>
-        }
-        <div className="flex-2">
-          <button onClick={handleClick} className="w-[120px] h-[40px] text-md mt-3 bg-blue-900 flex items-center justify-center gap-2 uppercase text-white rounded-sm hover:bg-blue-800"><FiSearch className=" scale-150"/>{language.search}</button>
+          <CalenderComponent
+            title={language.date}
+            value={calenderOne}
+            setValue={setCalenderOne}
+          />
+          <FuelTypeComponent
+            title={language.fuel_type}
+            value={fuelType}
+            setValue={setFuelType}
+          />
+          <TankComponent
+            language={language.tank_no}
+            value={tankName}
+            setValue={setTankName}
+          />
+          <StationComponent
+            title={language.station}
+            value={selectedStation}
+            setValue={setSelectedStation}
+          />
+        </div>
+        {isSelectedStation && (
+          <div className="flex mt-3 animate-[translate-y-6]   duration-200 text-blue-500 gap-[10px] justify-start text-[16px] items-center">
+            <FcInfo /> Please Select Station
           </div>
-       </InputContainer>
-                
-     
-      {
-        okData?.length > 0 ? <>
+        )}
+        <div className="flex-2">
+          <button
+            onClick={handleClick}
+            className="w-[120px] h-[40px] text-md mt-3 bg-blue-900 flex items-center justify-center gap-2 uppercase text-white rounded-sm hover:bg-blue-800"
+          >
+            <FiSearch className=" scale-150" />
+            {language.search}
+          </button>
+        </div>
+      </InputContainer>
+
+      {okData?.length > 0 ? (
+        <>
           {/* <FuelBalanceTable okData={okData} tableRef={tableRef} setOkData={setOkData} /> */}
-          <FuelBalanceReportTable language={language} tableRef={tableRef} okData={okData}/>
-        </> : ""
-       }
-              
-     
-     {
-        loading?<Loading/>:''
-      }
+          <FuelBalanceReportTable
+            language={language}
+            tableRef={tableRef}
+            okData={okData}
+          />
+        </>
+      ) : (
+        ""
+      )}
+
+      {loading ? <Loading /> : ""}
     </PageContainer>
-  )
+  );
 }
 
-export default FuelBalanceReport
+export default FuelBalanceReport;
